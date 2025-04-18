a0c648481879aad482ce0dbc4cf898088ab2995a
Varun G <varungadde@outlook.com>
2025-04-18 17:38:04 -0400

    test data setup with endpoint /api/test

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,14 @@
-print("hello world")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/test')
+def testData():
+    data = {"Garage": "North Deck",
+            "nowPercent": 0.73,
+            "estimatedParkingTime": 6}
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
